ff393e05979bce49f9ec05db9964b74fdba5a28f
mdepeiza1 <mdepeiza@keyconceptskb.com>
2021-03-31 11:30:45 -0400

    Youtube API added for To-Do column

---
+++ Start_To_Finish/wwwroot/js/script.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+    var API_KEY = $('#API_KEY').val()
+
+    var video = ''
+
+    $("#form").submit(function (event) {
+        event.preventDefault()
+
+        var search = $('#search').val()
+
+        videoSearch(API_KEY, search, 10)
+    })
+
+
+    function videoSearch(key, search, maxResults) {
+        $("#videos").empty()
+
+        $.get("https://www.googleapis.com/youtube/v3/search?key=" + key + "&type=video&part=snippet"
+            + "&maxResults=" + maxResults + "&q=" + search, function (data) {
+                console.log(data) 
+
+                data.items.forEach(item => {
+                    video = `
+                    <iframe width "200" height "200" src="https://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe> 
+
+
+                    `
+                    $("#videos").append(video)
+
+                });
+        })
+    }
+})
